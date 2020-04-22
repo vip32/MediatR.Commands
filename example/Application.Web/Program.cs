871b887237389e6e554f5d63c793cbaf4ee91b84
Vincent van Proosdij <mini32@gmail.com>
2020-04-22 10:07:10 +0200

    example

--- example/Application.Web/Program.cs
+++ example/Application.Web/Program.cs
@@ -1,20 +1,72 @@
-namespace Stateless.Web.Application
+namespace WeatherForecast.Application.Web
 {
+    using System;
+    using System.IO;
+    using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
+    using Serilog;
 
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static readonly string AppName = typeof(Program).Namespace;
+
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = GetConfiguration();
+            Log.Logger = CreateLogger(configuration);
+
+            try
+            {
+                Log.Information("configuring web host (service={ServiceName})...", AppName);
+                var host = BuildWebHost(configuration, args);
+
+                Log.Information("starting web host (service={ServiceName})...", AppName);
+                host.Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "program terminated unexpectedly (service={ServiceName})!", AppName);
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        private static IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .CaptureStartupErrors(false)
+                .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(configuration)
+                .UseSerilog()
+                .Build();
+
+        private static ILogger CreateLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ServiceName", AppName)
+                .Enrich.FromLogContext()
+                .WriteTo.Trace()
+                .WriteTo.Console()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
     }
 }
--- example/Application.Web/Startup.cs
+++ example/Application.Web/Startup.cs
@@ -1,4 +1,4 @@
-namespace Stateless.Web.Application
+namespace WeatherForecast.Application.Web
 {
     using global::Application;
     using MediatR;
