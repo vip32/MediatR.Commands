0127b3903639068253d57f71d26935f860a8320f
vip32 <mini32@gmail.com>
2020-07-24 19:25:14 +0200

    more CommandEndpointDocumentProcessor

--- src/MediatR.Commands.Web/Swagger/CommandEndpointDocumentProcessor.cs
+++ src/MediatR.Commands.Web/Swagger/CommandEndpointDocumentProcessor.cs
@@ -1,13 +1,37 @@
 ﻿namespace MediatR.Commands
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using NSwag;
     using NSwag.Generation.Processors;
     using NSwag.Generation.Processors.Contexts;
 
     public class CommandEndpointDocumentProcessor : IDocumentProcessor
     {
+        private readonly ICommandEndpointConfiguration configuration;
+
+        public CommandEndpointDocumentProcessor(ICommandEndpointConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void Process(DocumentProcessorContext context)
         {
-            throw new System.NotImplementedException();
+            if (this.configuration?.Registrations == null)
+            {
+                return;
+            }
+
+            foreach (var endpoint in this.configuration.Registrations
+                .Where(e => !e.Pattern.IsNullOrEmpty()).GroupBy(e => e.Pattern))
+            {
+                //AddPathItem(context.Document.Paths, registrations.DistinctBy(r => r.RequestMethod), context);
+            }
+        }
+
+        private static void AddPathItem(IDictionary<string, OpenApiPathItem> items, IEnumerable<CommandEndpointConfiguration> endpoints, DocumentProcessorContext context)
+        {
+            var item = new OpenApiPathItem();
         }
     }
 }
