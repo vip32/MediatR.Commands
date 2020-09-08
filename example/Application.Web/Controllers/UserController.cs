94c7de3b2f6e1f75579f843684f0a16d28b13098
Vincent van Proosdij <vincent.van.proosdij@bridging-it.de>
2020-09-08 08:21:52 +0200

    UserController

---
+++ example/Application.Web/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿namespace Stateless.Web.Application.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using global::Application;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UserController(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecastQueryResponse>> Get()
+        {
+            return await this.mediator.Send(new WeatherForecastsQuery()).ConfigureAwait(false);
+        }
+
+        [HttpGet]
+        [Route("{daysOffset:int}")]
+        public async Task<IEnumerable<WeatherForecastQueryResponse>> Get(int daysOffset)
+        {
+            return await this.mediator.Send(
+                new WeatherForecastsQuery(daysOffset)).ConfigureAwait(false);
+        }
+    }
+}
