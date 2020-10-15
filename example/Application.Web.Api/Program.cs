eb5441054f6abde320b7ff3d1602e0d94ebc5e32
vip32 <mini32@gmail.com>
2020-10-15 22:15:04 +0200

    better program startup

--- example/Application.Web.Api/Program.cs
+++ example/Application.Web.Api/Program.cs
@@ -1,9 +1,7 @@
 namespace Application.Web.Aapi
 {
-    using System;
     using System.IO;
     using Application.Web.Api;
-    using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
@@ -13,61 +11,31 @@ namespace Application.Web.Aapi
     {
         public static readonly string AppName = typeof(Program).Namespace;
 
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            var configuration = GetConfiguration();
-            Log.Logger = CreateLogger(configuration);
-
-            try
-            {
-                Log.Information("configuring web host (service={ServiceName})...", AppName);
-                var host = BuildWebHost(configuration, args);
-
-                Log.Information("starting web host (service={ServiceName})...", AppName);
-                host.Run();
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "program terminated unexpectedly (service={ServiceName})!", AppName);
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
-        private static IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .CaptureStartupErrors(false)
-                .UseStartup<Startup>()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseConfiguration(configuration)
-                .UseSerilog()
-                .Build();
-
-        private static ILogger CreateLogger(IConfiguration configuration)
-        {
-            return new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .Enrich.WithProperty("ServiceName", AppName)
-                .Enrich.FromLogContext()
-                .WriteTo.Trace()
-                .WriteTo.Console()
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
-        }
-
-        private static IConfiguration GetConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
-
-            return builder.Build();
-        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration((context, builder) =>
+               {
+                   builder
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{context?.HostingEnvironment?.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+               })
+               .UseSerilog((context, builder) =>
+               {
+                    builder
+                      .ReadFrom.Configuration(context.Configuration)
+                      .MinimumLevel.Verbose()
+                      .Enrich.WithProperty("ServiceName", AppName)
+                      .Enrich.FromLogContext()
+                      .WriteTo.Trace()
+                      .WriteTo.Console();
+               })
+               .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
