221332916b290ae76a066bac82f11ec9a78cf9f9
vp <vp>
2020-11-14 20:56:44 +0100

    undo

--- build/Build.cs
+++ build/Build.cs
@@ -21,14 +21,14 @@ using static Nuke.Common.Tools.NSwag.NSwagTasks;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
-[GitHubActions("github-actions",
-    GitHubActionsImage.UbuntuLatest,
-    AutoGenerate = true,
-    PublishArtifacts = true,
-    InvokedTargets = new[] { nameof(Test), nameof(Pack) },
-    OnPushBranches = new[] { "master", "develop", "refs/tags/v*" },
-    OnPullRequestBranches = new[] { "features/*" },
-    ImportSecrets = new[] { nameof(NugetApiKey) })]
+// [GitHubActions("github-actions",
+//     GitHubActionsImage.UbuntuLatest,
+//     AutoGenerate = true,
+//     PublishArtifacts = true,
+//     InvokedTargets = new[] { nameof(Test), nameof(Pack) },
+//     OnPushBranches = new[] { "master", "develop", "refs/tags/v*" },
+//     OnPullRequestBranches = new[] { "features/*" },
+//     ImportSecrets = new[] { nameof(NugetApiKey) })]
 //[AzurePipelines(
 //    AzurePipelinesImage.UbuntuLatest,
 //    AutoGenerate = true,
