60d4ec4976cdda9e15ad6cfabcb5ec16decb6a74
vp <vp>
2020-12-23 13:36:17 +0100

    cached query

--- example/Application/Queries/UserFindAllQuery.cs
+++ example/Application/Queries/UserFindAllQuery.cs
@@ -1,9 +1,15 @@
 ﻿namespace Application
 {
+    using System;
     using System.Collections.Generic;
     using MediatR.Commands;
 
-    public class UserFindAllQuery : QueryBase<IEnumerable<User>>
+    public class UserFindAllQuery : QueryBase<IEnumerable<User>>, ICachedQuery
     {
+        public string CacheKey => nameof(UserFindAllQuery);
+
+        public TimeSpan? SlidingExpiration => new TimeSpan(0, 0, 10);
+
+        public DateTimeOffset? AbsoluteExpiration => null;
     }
 }
--- example/Application/Queries/UserFindByIdQuery.cs
+++ example/Application/Queries/UserFindByIdQuery.cs
@@ -1,8 +1,9 @@
 ﻿namespace Application
 {
+    using System;
     using MediatR.Commands;
 
-    public class UserFindByIdQuery : QueryBase<User>
+    public class UserFindByIdQuery : QueryBase<User>, ICachedQuery
     {
         public UserFindByIdQuery()
         {
@@ -14,5 +15,11 @@
         }
 
         public string UserId { get; set; }
+
+        public string CacheKey => nameof(UserFindByIdQuery);
+
+        public TimeSpan? SlidingExpiration => new TimeSpan(0, 0, 10);
+
+        public DateTimeOffset? AbsoluteExpiration => null;
     }
 }
